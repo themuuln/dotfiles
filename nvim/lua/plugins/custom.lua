12ff291fe2ca9ede106b0803f4b04eeba22eaff9
ICT <ict@Temuulen-MacBook-Pro.local>
2024-12-04 15:31:32 +0800

    kulala added

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -5,7 +5,29 @@ return {
     event = "BufEnter",
   },
 
-  { "epwalsh/obsidian.nvim", enabled = true },
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --   version = "*",
+  --   lazy = true,
+  --   ft = "markdown",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   opts = {
+  --     workspaces = {
+  --       {
+  --         name = "personal",
+  --         path = "/Users/ict/Library/Mobile Documents/iCloud~md~obsidian/Documents/home/notes",
+  --       },
+  --       {
+  --         name = "work",
+  --         path = "~/vaults/work",
+  --       },
+  --     },
+  --
+  --     -- see below for full list of options 👇
+  --   },
+  -- },
 
   {
     "hrsh7th/nvim-cmp",
@@ -169,4 +191,78 @@ return {
   --   end,
   --   event = "VeryLazy", -- Load the plugin during startup for optimal performance
   -- },
+  {
+    "mistweaverco/kulala.nvim",
+    ft = "http",
+    keys = {
+      { "<leader>R", "", desc = "+Rest", ft = "http" },
+      { "<leader>Rb", "<cmd>lua require('kulala').scratchpad()<cr>", desc = "Open scratchpad", ft = "http" },
+      { "<leader>Rc", "<cmd>lua require('kulala').copy()<cr>", desc = "Copy as cURL", ft = "http" },
+      { "<leader>RC", "<cmd>lua require('kulala').from_curl()<cr>", desc = "Paste from curl", ft = "http" },
+      {
+        "<leader>Rg",
+        "<cmd>lua require('kulala').download_graphql_schema()<cr>",
+        desc = "Download GraphQL schema",
+        ft = "http",
+      },
+      { "<leader>Ri", "<cmd>lua require('kulala').inspect()<cr>", desc = "Inspect current request", ft = "http" },
+      { "<leader>Rn", "<cmd>lua require('kulala').jump_next()<cr>", desc = "Jump to next request", ft = "http" },
+      { "<leader>Rp", "<cmd>lua require('kulala').jump_prev()<cr>", desc = "Jump to previous request", ft = "http" },
+      { "<leader>Rq", "<cmd>lua require('kulala').close()<cr>", desc = "Close window", ft = "http" },
+      { "<leader>Rr", "<cmd>lua require('kulala').replay()<cr>", desc = "Replay the last request", ft = "http" },
+      { "<leader>Rs", "<cmd>lua require('kulala').run()<cr>", desc = "Send the request", ft = "http" },
+      { "<leader>RS", "<cmd>lua require('kulala').show_stats()<cr>", desc = "Show stats", ft = "http" },
+      { "<leader>Rt", "<cmd>lua require('kulala').toggle_view()<cr>", desc = "Toggle headers/body", ft = "http" },
+    },
+    opts = {
+      -- split, float
+      display_mode = "split",
+
+      split_direction = "horizontal",
+
+      default_view = "body",
+      -- default_view, body or headers or headers_body
+
+      -- dev, test, prod, can be anything
+      default_env = "test",
+
+      -- additional cURL options
+      -- see: https://curl.se/docs/manpage.html
+      additional_curl_options = {},
+
+      -- TODO: Update default contents based on current environment
+      scratchpad_default_contents = {
+        "@MY_TOKEN_NAME=my_token_value",
+        "",
+        "# @name scratchpad",
+        "POST https://httpbin.org/post HTTP/1.1",
+        "accept: application/json",
+        "content-type: application/json",
+        "",
+        "{",
+        '  "foo": "bar"',
+        "}",
+      },
+
+      -- enable winbar
+      winbar = true,
+
+      -- Specify the panes to be displayed by default
+      default_winbar_panes = { "body", "headers", "headers_body", "script_output" },
+
+      -- enable reading vscode rest client environment variables
+      vscode_rest_client_environmentvars = false,
+
+      -- disable the vim.print output of the scripts
+      -- they will be still written to disk, but not printed immediately
+      disable_script_print_output = false,
+
+      -- set scope for environment and request variables
+      -- possible values: b = buffer, g = global
+      environment_scope = "b",
+
+      -- certificates
+      certificates = {},
+    },
+  },
 }
