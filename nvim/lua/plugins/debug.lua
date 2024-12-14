b1a30ea6e8cc36cf2c0f5f616e8ca5c6e111e2ac
ICT <ict@Temuulen-MacBook-Pro.local>
2024-12-14 13:36:07 +0800

    debug is now working

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -5,6 +5,7 @@ return {
     event = "VeryLazy",
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
     },
     ft = "dart",
     config = function()
@@ -12,9 +13,17 @@ return {
         flutter_path = nil,
         fvm = false,
         widget_guides = { enabled = true },
+        log = {
+          enabled = true,
+          open_cmd = "tabedit",
+        },
         dev_log = {
           enabled = true,
-          open_cmd = "tabnew",
+          open_cmd = "tabedit", -- ensure a valid command
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
         },
         lsp = {
           settings = {
@@ -32,68 +41,6 @@ return {
       })
     end,
   },
-  -- {
-  --   "akinsho/flutter-tools.nvim",
-  --   event = "VeryLazy",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "stevearc/dressing.nvim",
-  --   },
-  --   config = function()
-  --     require("flutter-tools").setup({
-  --       flutter_path = "/Users/ict/development/flutter/bin/flutter",
-  --       -- flutter_lookup_cmd = "asdf where flutter",
-  --       widget_guides = { enabled = true },
-  --       lsp = {
-  --         settings = {
-  --           lineLength = 100,
-  --           showtodos = true,
-  --           completefunctioncalls = true,
-  --           analysisexcludedfolders = {
-  --             vim.fn.expand("$Home/.pub-cache"),
-  --           },
-  --           renamefileswithclasses = "prompt",
-  --           updateimportsonrename = true,
-  --           enablesnippets = false,
-  --         },
-  --       },
-  --       debugger = {
-  --         enabled = true,
-  --         run_via_dap = true,
-  --         exception_breakpoints = {},
-  --         register_configurations = function()
-  --           local dap = require("dap")
-  --           dap.adapters.dart = {
-  --             type = "executable",
-  --             command = "/Users/ict/development/flutter/bin/flutter",
-  --             args = { "debug-adapter" },
-  --             options = {
-  --               detached = false,
-  --             },
-  --           }
-  --           dap.configurations.dart = {
-  --             {
-  --               type = "dart",
-  --               request = "launch",
-  --               name = "Launch Dart Program",
-  --               program = "${workspaceFolder}/lib/main.dart",
-  --               cwd = "${workspaceFolder}",
-  --             },
-  --           }
-  --           require("dap.ext.vscode").load_launchjs()
-  --         end,
-  --       },
-  --     })
-  --   end,
-  -- },
-  {
-    "dart-lang/dart-vim-plugin",
-    ft = "dart",
-    config = function()
-      vim.g.dart_enable_hover = 1
-      vim.g.dart_sdk_path = "/Users/ict/development/flutter/bin/cache/dart-sdk"
-    end,
-  },
   {
     "mfussenegger/nvim-dap",
     dependencies = {
@@ -102,21 +49,42 @@ return {
     },
     event = "VeryLazy",
     config = function()
-      require("dapui").setup({
-        icons = { expanded = "▾", collapsed = "▸" },
-        layouts = {
-          {
-            elements = {
-              { id = "scopes", size = 0.25 },
-              "breakpoints",
-              "stacks",
-              "watches",
-            },
-            size = 10, -- columns
-            position = "bottom",
-          },
+      local dap = require("dap")
+
+      -- Define the Dart adapter
+      dap.adapters.dart = {
+        type = "executable",
+        command = "/Users/ict/development/flutter/bin/flutter",
+        args = { "debug_adapter" },
+      }
+
+      -- Flutter DAP Configurations
+      dap.configurations.dart = {
+        -- Remove Comoments for Dart Development
+        -- {
+        --   type = "dart",
+        --   request = "launch",
+        --   name = "Launch Dart",
+        --   dartSdkPath = "/Users/ict/development/flutter/bin/cache/dart-sdk/bin/dart",
+        --   flutterSdkPath = "/Users/ict/development/flutter/bin",
+        --   program = "${workspaceFolder}/lib/main.dart",
+        --   cwd = "${workspaceFolder}",
+        -- },
+        {
+          type = "dart",
+          request = "launch",
+          name = "Launch Flutter",
+          dartSdkPath = "/Users/ict/development/flutter/bin/cache/dart-sdk/bin/dart",
+          flutterSdkPath = "/Users/ict/development/flutter/bin",
+          program = "${workspaceFolder}/lib/main.dart",
+          cwd = "${workspaceFolder}",
+          -- toolArgs = {
+          --   "-d",
+          --   "abae108eb0eacccc44374baf2b91a79bd972e5a5", -- Device ID
+          --   "--debug",
+          -- },
         },
-      })
+      }
     end,
   },
   {
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -283,15 +283,9 @@ return {
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
   },
   -- colorschemes
-  { "ellisonleao/gruvbox.nvim" },
   { "olimorris/onedarkpro.nvim" },
   { "AlexvZyl/nordic.nvim" },
   { "sainnhe/everforest" },
-  { "neanias/everforest-nvim" },
   { "navarasu/onedark.nvim" },
-  { "marko-cerovac/material.nvim" },
-  { "Mofiqul/vscode.nvim" },
-  { "sainnhe/sonokai" },
   { "craftzdog/solarized-osaka.nvim" },
-  { "rebelot/kanagawa.nvim" },
 }
