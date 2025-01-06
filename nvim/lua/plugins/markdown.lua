121685db903a643e1edad173e590a9af76914136
themuuln <zerone.offical@gmail.com>
2025-01-06 16:46:32 +0800

    setup obsidian

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -7,6 +7,18 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    opts = {
+      workspaces = {
+        {
+          name = "coding",
+          path = "/Users/ict/Library/Mobile Documents/iCloud~md~obsidian/Documents/Home/notes",
+        },
+      },
+      notes_subdir = "nvim",
+      new_notes_location = "nvim_subdir",
+
+      disable_frontmatter = true,
+    },
   },
   { "preservim/vim-markdown" },
   { "epwalsh/pomo.nvim" },
