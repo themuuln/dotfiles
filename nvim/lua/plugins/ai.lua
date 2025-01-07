2a127eabb4594891bbf210117b3dd89f7fbef79b
themuuln <zerone.offical@gmail.com>
2025-01-07 16:59:36 +0800

    rename supermaven => ai

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,50 @@
+return {}
+
+-- OLD
+-- return {
+--   -- {
+--   --   "supermaven-inc/supermaven-nvim",
+--   --   opts = function()
+--   --     require("supermaven-nvim.completion_preview").suggestion_group = "SupermavenSuggestion"
+--   --     LazyVim.cmp.actions.ai_accept = function()
+--   --       local suggestion = require("supermaven-nvim.completion_preview")
+--   --       if suggestion.has_suggestion() then
+--   --         LazyVim.create_undo()
+--   --         vim.schedule(function()
+--   --           suggestion.on_accept_suggestion()
+--   --         end)
+--   --         return true
+--   --       end
+--   --     end
+--   --   end,
+--   -- },
+--   {
+--     "saghen/blink.cmp",
+--     optional = true,
+--     dependencies = { "supermaven-nvim", "saghen/blink.compat" },
+--     opts = {
+--       sources = {
+--         compat = { "supermaven" },
+--         providers = { supermaven = { kind = "Supermaven" } },
+--       },
+--     },
+--   },
+--   {
+--     "folke/noice.nvim",
+--     optional = true,
+--     opts = function(_, opts)
+--       vim.list_extend(opts.routes, {
+--         {
+--           filter = {
+--             event = "msg_show",
+--             any = {
+--               { find = "Starting Supermaven" },
+--               { find = "Supermaven Free Tier" },
+--             },
+--           },
+--           skip = true,
+--         },
+--       })
+--     end,
+--   },
+-- }
