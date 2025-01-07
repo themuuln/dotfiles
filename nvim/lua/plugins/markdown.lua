2d439025376fa224504ca3105d4f1f1d425fc010
themuuln <zerone.offical@gmail.com>
2025-01-07 18:02:24 +0800

    obsidian fixed

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -4,21 +4,7 @@ return {
     version = "*",
     lazy = true,
     ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "coding",
-          path = "/Users/ict/Library/Mobile Documents/iCloud~md~obsidian/Documents/Home/notes",
-        },
-      },
-      notes_subdir = "nvim",
-      new_notes_location = "nvim_subdir",
-
-      disable_frontmatter = true,
-    },
+    dependencies = { "nvim-lua/plenary.nvim" },
   },
   { "preservim/vim-markdown" },
   { "epwalsh/pomo.nvim" },
