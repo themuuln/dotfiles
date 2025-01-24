d2c9fced8acf8c71bea53e4b8d1d25a8281913e3
themuuln <zerone.offical@gmail.com>
2025-01-24 16:57:40 +0800

    themes removed

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -2,19 +2,13 @@ return {
   {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
-    opts = {
-      plugins = {
-        gitsigns = true,
-        tmux = true,
-        kitty = { enabled = false, font = "+2" },
-      },
-    },
+    opts = { plugins = { gitsigns = true, tmux = true, kitty = { enabled = false, font = "+2" } } },
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
   },
   -- colorschemes
-  { "olimorris/onedarkpro.nvim" },
-  { "AlexvZyl/nordic.nvim" },
-  { "sainnhe/everforest" },
-  { "navarasu/onedark.nvim" },
-  { "craftzdog/solarized-osaka.nvim" },
+  -- { "olimorris/onedarkpro.nvim" },
+  -- { "AlexvZyl/nordic.nvim" },
+  -- { "sainnhe/everforest" },
+  -- { "navarasu/onedark.nvim" },
+  -- { "craftzdog/solarized-osaka.nvim" },
 }
