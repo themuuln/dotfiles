672b6e3dfc2ba564d5c2b063eacf43f99253e1d1
themuuln <zerone.offical@gmail.com>
2025-01-27 16:14:04 +0800

    codeium moved

--- nvim/lua/plugins/which-key.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/which-key.nvim",
-  opts = {
-    defaults = {
-      -- TODO: only for flutter (for now)
-      ["<leader>dy"] = { ":FlutterLogToggle<CR>", "Toggle Flutter Logs" },
-    },
-  },
-}
