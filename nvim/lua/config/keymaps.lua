f88f3372937ae36d17d603dd1831d01340a67fb5
themuuln <zerone.offical@gmail.com>
2025-01-28 18:11:13 +0800

    flutter commands added on whichkey

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
--- Jumplist
+local wk = require("which-key")
+
 vim.keymap.set("n", "<C-m>", "<C-i>", { noremap = true, silent = true })
 vim.keymap.set("n", "U", ":redo<CR>", { noremap = true, silent = true })
+
+wk.add({
+  { "<leader>dd", "<cmd>FlutterDebug<cr>", desc = "Debug Flutter App", group = "Debug Flutter App", mode = "n" },
+
+  {
+    "<leader>F",
+    group = "flutter",
+    expand = function()
+      return require("which-key.extras").expand.buf()
+    end,
+  },
+  { "<leader>Fs", "<cmd>FlutterRun<cr>", desc = "Run Flutter App", group = "Run Flutter App", mode = "n" },
+  { "<leader>Fr", "<cmd>FlutterRestart<cr>", desc = "Restart Flutter App", group = "Restart Flutter App", mode = "n" },
+})
