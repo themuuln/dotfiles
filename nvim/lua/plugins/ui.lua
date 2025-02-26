405c97ca40b5eeec190b0fc536ac2de29071ed09
themuuln <zerone.offical@gmail.com>
2025-02-26 16:19:53 +0800

    feat: incline removedd

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -3,26 +3,6 @@ return {
     "folke/tokyonight.nvim",
     opts = { transparent = true, styles = { sidebars = "transparent", floats = "transparent" } },
   },
-  {
-    "b0o/incline.nvim",
-    event = "BufReadPre",
-    priority = 1200,
-    config = function()
-      require("incline").setup({
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+] " .. filename
-          end
-
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { filename } }
-        end,
-      })
-    end,
-  },
-  -- colorschemes
   { "olimorris/onedarkpro.nvim" },
   { "AlexvZyl/nordic.nvim" },
   { "sainnhe/everforest" },
