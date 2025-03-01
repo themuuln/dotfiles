b118ba611bc778680a4036780f64134d2d027af5
themuuln <zerone.offical@gmail.com>
2025-03-01 21:22:50 +0800

    deprecated opts removed

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -6,7 +6,6 @@ return {
       formatters = {
         dart_format = { command = "dart", args = { "format", "--line-length", "120", "$FILENAME" }, stdin = false },
       },
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
     },
   },
 }
