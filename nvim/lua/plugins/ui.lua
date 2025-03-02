3a5edb9173ca96591ffa11a7be5313ebafa13070
themuuln <zerone.offical@gmail.com>
2025-03-02 22:08:18 +0800

    nordic transparent, github added

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,9 +1,19 @@
 return {
   { "folke/tokyonight.nvim", opts = { transparent = true } },
   { "olimorris/onedarkpro.nvim" },
-  { "AlexvZyl/nordic.nvim" },
+  { "AlexvZyl/nordic.nvim", opts = { transparent = { bg = true, float = true } } },
   { "sainnhe/everforest" },
   { "navarasu/onedark.nvim" },
   { "craftzdog/solarized-osaka.nvim" },
   { "rebelot/kanagawa.nvim" },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({ options = { transparent = true } })
+      vim.cmd("colorscheme github_dark_dimmed")
+    end,
+  },
 }
