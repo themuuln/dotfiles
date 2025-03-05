dd6420aa631b9faf67f718ddac7e1bc91296830e
themuuln <zerone.offical@gmail.com>
2025-03-05 20:13:51 +0800

    custom print macro added on js

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,18 @@
 require("config.lazy")
+
+-- c  "l   yoprint('^[pa: ${^[pa}^[A;^[
+------------------
+-- CUSTOM MACRO --
+------------------
+
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+
+vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = "JSLogMacro",
+  pattern = { "javascript", "typescript" },
+  callback = function()
+    vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
+  end,
+})
