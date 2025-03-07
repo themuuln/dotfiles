493f64d76107798bbeaffe0d3c0319d2d41e6f97
themuuln <zerone.offical@gmail.com>
2025-03-07 14:33:42 +0800

    feat(macro): add default log macro

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -8,11 +8,20 @@ require("config.lazy")
 local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
 
 vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+vim.api.nvim_create_augroup("DartLogMacro", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
   group = "JSLogMacro",
   pattern = { "javascript", "typescript" },
   callback = function()
     vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = "DartLogMacro",
+  pattern = { "dart" },
+  callback = function()
+    vim.fn.setreg("l", "yoprint('" .. esc .. "pa ${" .. esc .. "la}" .. esc .. "pl")
   end,
 })
