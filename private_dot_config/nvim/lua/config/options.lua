730da86c31adabf650dd599ec857a41ec688296a
themuuln <zerone.offical@gmail.com>
2025-03-08 18:38:33 +0800

    feat(nvim): increment, decrement command keybind added

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,23 @@ vim.g.snacks_animate = false
 vim.o.scrolloff = 8
 vim.opt.cursorcolumn = true
 vim.opt.smartindent = true
+
+---------------------------------------------------------------
+-- Additional dial.nvim keybinds, conflicts with tmux prefix --
+---------------------------------------------------------------
+
+-- Normal Mode Mappings
+vim.keymap.set("n", "+", function()
+  require("dial.map").manipulate("increment", "normal")
+end)
+vim.keymap.set("n", "-", function()
+  require("dial.map").manipulate("decrement", "normal")
+end)
+
+-- Optional: Visual Mode Mappings
+vim.keymap.set("v", "+", function()
+  require("dial.map").manipulate("increment", "visual")
+end)
+vim.keymap.set("v", "-", function()
+  require("dial.map").manipulate("decrement", "visual")
+end)
