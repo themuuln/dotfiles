6d185830412b083b82c4f59d007bdca736b4fc9f
themuuln <zerone.offical@gmail.com>
2025-03-18 12:02:01 +0800

    feat: crsnip.nvim added

--- private_dot_config/nvim/lua/plugins/coding.lua
+++ private_dot_config/nvim/lua/plugins/coding.lua
@@ -10,4 +10,18 @@ return {
     },
   },
   { "mg979/vim-visual-multi" },
+  {
+    "themuuln/crsnip.nvim",
+    config = function()
+      require("crsnip").setup({
+        options = {
+          snippet_dir = vim.fn.stdpath("config") .. "/snippets",
+          debug = true,
+        },
+      })
+
+      vim.keymap.set("n", "<leader>cb", "<cmd>CRSnip<CR>", { desc = "Create snippet" })
+      vim.keymap.set("v", "<leader>cb", ":CRSnip<CR>", { desc = "Create snippet from selection" })
+    end,
+  },
 }
