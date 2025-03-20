6d8248ee2158411e9b3bb1bddd2f846342b0576a
themuuln <zerone.offical@gmail.com>
2025-03-20 11:44:35 +0800

    feat: jk, jj to esc

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@ local wk = require("which-key")
 
 vim.keymap.set("n", "<C-m>", "<C-i>", { noremap = true, silent = true })
 vim.keymap.set("n", "U", ":redo<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = false })
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = false })
 
 wk.add({
   { "<leader>dd", "<cmd>FlutterDebug<cr>", desc = "Debug Flutter App", group = "Debug Flutter App", mode = "n" },
