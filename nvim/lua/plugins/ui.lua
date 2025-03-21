7cdebcdcbba9cebe49ddf995fc2d2b16cdb5f810
themuuln <zerone.offical@gmail.com>
2025-03-21 23:19:35 +0800

    feat: lualine integrated with supermaven

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -113,4 +113,12 @@ return {
     },
   },
   { "rmehri01/onenord.nvim" },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("supermaven"))
+    end,
+  },
 }
