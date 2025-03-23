c4f583f303cc83206b9e196e4f9842dae343b326
themuuln <zerone.offical@gmail.com>
2025-03-23 21:43:02 +0800

    few themes

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -4,7 +4,9 @@ return {
     "LazyVim/LazyVim",
     -- opts = { colorscheme = "github_dark_dimmed" },
     -- opts = { colorscheme = "nordic" },
-    opts = { colorscheme = "kanagawa-wave" },
+    -- opts = { colorscheme = "kanagawa-wave" },
+    opts = { colorscheme = "catppuccin-macchiato" },
+    -- opts = { colorscheme = "catppuccin-frappe" },
   },
 
   {
