46e2673deea24c1d46de256b371cbc71ef6e0f6f
themuuln <zerone.offical@gmail.com>
2025-03-23 21:42:42 +0800

    feat(nvim): mini-move addedw with custom keymap

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -116,4 +116,22 @@ return {
       require("blink.cmp").setup(opts)
     end,
   },
+
+  {
+    "echasnovski/mini.move",
+    version = false,
+    opts = {
+      mappings = {
+        left = "<A-Left>",
+        right = "<A-Right>",
+        down = "<A-Down>",
+        up = "<A-Up>",
+
+        line_left = "<A-Left>",
+        line_right = "<A-Right>",
+        line_down = "<A-Down>",
+        line_up = "<A-Up>",
+      },
+    },
+  },
 }
