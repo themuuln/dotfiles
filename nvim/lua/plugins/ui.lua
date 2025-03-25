13b6ccdf8ace342eeb0354e336b86bffec0cddb0
themuuln <zerone.offical@gmail.com>
2025-03-25 08:48:36 +0800

    feat: incline file icon added

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -12,7 +12,29 @@ return {
   {
     "b0o/incline.nvim",
     config = function()
-      require("incline").setup()
+      require("incline").setup(require("incline").setup({
+        window = {
+          padding = 0,
+          margin = { horizontal = 0 },
+        },
+        render = function(props)
+          local helpers = require("incline.helpers")
+          local devicons = require("nvim-web-devicons")
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if filename == "" then
+            filename = "[No Name]"
+          end
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+          return {
+            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+            " ",
+            { filename, gui = modified and "bold,italic" or "bold" },
+            " ",
+            guibg = "#44406e",
+          }
+        end,
+      }))
     end,
     event = "VeryLazy",
   },
