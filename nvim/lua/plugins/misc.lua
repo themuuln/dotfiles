ec0e8409d25e72c9ad8a46c532a67418547bda1f
themuuln <zerone.offical@gmail.com>
2025-03-25 08:49:07 +0800

    feat(nvim): copy with context plugin added

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -20,4 +20,17 @@ return {
   --   cmd = { "Typr", "TyprStats" },
   -- },
   { "andweeb/presence.nvim" },
+  {
+    "zhisme/copy_with_context.nvim",
+    config = function()
+      require("copy_with_context").setup({
+        mappings = {
+          relative = "<leader>cy",
+          absolute = "<leader>cY",
+        },
+        trim_lines = true,
+        context_format = "# %s:%s", -- Default format for context: "# Source file: filepath:line"
+      })
+    end,
+  },
 }
