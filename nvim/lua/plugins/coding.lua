52c1358711ea8eb1126021a5d9f2d2fdbc329b6b
themuuln <zerone.offical@gmail.com>
2025-04-14 15:46:53 +0800

    feat: blink.nvim reconfigure

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,13 +1,7 @@
 return {
   {
     "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        mode = "tabs",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-      },
-    },
+    opts = { options = { mode = "tabs", show_buffer_close_icons = false, show_close_icon = false } },
   },
   { "mg979/vim-visual-multi" },
   {
@@ -30,110 +24,22 @@ return {
     opts = {
       cmdline = { enabled = false },
       signature = { enabled = true },
-      -- sources = {
-      --   default = { "lsp", "path", "snippets" },
-      -- },
-      sources = {
-        compat = { "supermaven" },
-        providers = {
-          supermaven = {
-            kind = "Supermaven",
-            score_offset = 100,
-            async = true,
-          },
-        },
-      },
       completion = {
         keyword = { range = "full" },
-        list = { selection = { preselect = false, auto_insert = true } },
+        list = { selection = { preselect = true, auto_insert = true } },
         accept = { auto_brackets = { enabled = true } },
         menu = {
           auto_show = true,
           draw = {
-
             treesitter = { "lsp" },
-            columns = {
-              { "label", "label_description", gap = 1 },
-              { "kind_icon", "kind" },
-            },
+            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
           },
         },
-
-        documentation = { auto_show = true, auto_show_delay_ms = 200 },
-        ghost_text = { enabled = vim.g.ai_cmp },
+        documentation = { auto_show = true, auto_show_delay_ms = 0 },
+        ghost_text = { enabled = true },
       },
-      -- keymap = {
-      --   preset = "enter",
-      --   ["<C-y>"] = { "select_and_accept" },
-      -- },
     },
-    ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
-    config = function(_, opts)
-      -- setup compat sources
-      local enabled = opts.sources.default
-      for _, source in ipairs(opts.sources.compat or {}) do
-        opts.sources.providers[source] = vim.tbl_deep_extend(
-          "force",
-          { name = source, module = "blink.compat.source" },
-          opts.sources.providers[source] or {}
-        )
-        if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
-          table.insert(enabled, source)
-        end
-      end
-
-      -- add ai_accept to <Tab> key
-      if not opts.keymap["<Tab>"] then
-        if opts.keymap.preset == "super-tab" then -- super-tab
-          opts.keymap["<Tab>"] = {
-            require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
-            LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-            "fallback",
-          }
-        else -- other presets
-          opts.keymap["<Tab>"] = {
-            LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-            "fallback",
-          }
-        end
-      end
-
-      -- Unset custom prop to pass blink.cmp validation
-      opts.sources.compat = nil
-
-      -- check if we need to override symbol kinds
-      for _, provider in pairs(opts.sources.providers or {}) do
-        ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
-        if provider.kind then
-          local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-          local kind_idx = #CompletionItemKind + 1
-
-          CompletionItemKind[kind_idx] = provider.kind
-          ---@diagnostic disable-next-line: no-unknown
-          CompletionItemKind[provider.kind] = kind_idx
-
-          ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
-          local transform_items = provider.transform_items
-          ---@param ctx blink.cmp.Context
-          ---@param items blink.cmp.CompletionItem[]
-          provider.transform_items = function(ctx, items)
-            items = transform_items and transform_items(ctx, items) or items
-            for _, item in ipairs(items) do
-              item.kind = kind_idx or item.kind
-              item.kind_icon = LazyVim.config.icons.kinds[item.kind_name] or item.kind_icon or nil
-            end
-            return items
-          end
-
-          -- Unset custom prop to pass blink.cmp validation
-          provider.kind = nil
-        end
-      end
-
-      require("blink.cmp").setup(opts)
-    end,
   },
-
   {
     "echasnovski/mini.move",
     version = false,
@@ -151,7 +57,5 @@ return {
       },
     },
   },
-  {
-    "sindrets/diffview.nvim",
-  },
+  { "sindrets/diffview.nvim" },
 }
