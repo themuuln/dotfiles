c8b3ac61b651096c66d9f2e2a9951069fcf30964
themuuln <zerone.offical@gmail.com>
2025-04-14 10:28:13 +0800

    feat(nvim): debug ui improvement

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,6 +6,32 @@ vim.opt.smartindent = true
 vim.opt.laststatus = 3
 -- -- vim insert mode cursor style is now `block`
 -- vim.opt.guicursor = ""
+vim.opt.formatoptions:append({ "r" })
+vim.g.lazyvim_cmp = "blink.cmp"
+vim.opt.cursorcolumn = false
+
+local namespace = vim.api.nvim_create_namespace("dap-hlng")
+vim.api.nvim_set_hl(namespace, "DapBreakpoint", { fg = "#eaeaeb", bg = "#ffffff" })
+vim.api.nvim_set_hl(namespace, "DapLogPoint", { fg = "#eaeaeb", bg = "#ffffff" })
+vim.api.nvim_set_hl(namespace, "DapStopped", { fg = "#eaeaeb", bg = "#ffffff" })
+
+vim.fn.sign_define(
+  "DapBreakpoint",
+  { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+  "DapBreakpointCondition",
+  { text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+  "DapBreakpointRejected",
+  { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+  "DapLogPoint",
+  { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+)
+vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
 
 ---------------------------------------------------------------
 -- Additional dial.nvim keybinds, conflicts with tmux prefix --
