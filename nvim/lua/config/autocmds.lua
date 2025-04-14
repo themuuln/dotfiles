bfc40dc6e8b0cb802c6dbe7d4359fc298358a90f
themuuln <zerone.offical@gmail.com>
2025-04-14 10:27:22 +0800

    ui(nvim): nvim

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,7 @@
+-- disable conceal level on json
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.opt.conceallevel = 0
+  end,
+})
