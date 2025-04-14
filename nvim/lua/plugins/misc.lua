8ecf80eabf74591069c561ec337e4120f9f027bc
themuuln <zerone.offical@gmail.com>
2025-04-14 15:47:04 +0800

    fix: kulala removed

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,17 +1,17 @@
 return {
   -- for http requests postman, insomnia alternative
-  {
-    "mistweaverco/kulala.nvim",
-    keys = {
-      {
-        "<leader>Ra",
-        "<cmd>lua require('kulala').set_selected_env()<cr>",
-        desc = "Set selected request as current",
-        ft = "http",
-      },
-    },
-    opts = { default_env = "test" },
-  },
+  -- {
+  --   "mistweaverco/kulala.nvim",
+  --   keys = {
+  --     {
+  --       "<leader>Ra",
+  --       "<cmd>lua require('kulala').set_selected_env()<cr>",
+  --       desc = "Set selected request as current",
+  --       ft = "http",
+  --     },
+  --   },
+  --   opts = { default_env = "test" },
+  -- },
   -- -- Typr for warm-up
   -- {
   --   "nvzone/typr",
