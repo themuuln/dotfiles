f872179e4716ce0aea1c1520f0e679e974511d05
themuuln <zerone.offical@gmail.com>
2025-04-15 11:51:17 +0800

    feat(nvim): added themes

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,15 +1,17 @@
 return {
-  -- set default theme here
   {
     "LazyVim/LazyVim",
     -- opts = { colorscheme = "github_dark_dimmed" },
-    opts = { colorscheme = "nordic" },
+    -- opts = { colorscheme = "nordic" },
+    -- opts = { colorscheme = "everforest" },
+    -- opts = { colorscheme = "zenbones" },
+    -- opts = { colorscheme = "tokyonight" },
     -- opts = { colorscheme = "kanagawa-wave" },
+    -- opts = { colorscheme = "solarized-osaka" },
     -- opts = { colorscheme = "catppuccin-macchiato" },
-    -- opts = { colorscheme = "catppuccin-frappe" },
+    opts = { colorscheme = "catppuccin-frappe" },
     -- opts = { colorscheme = "vscode" },
   },
-
   {
     "b0o/incline.nvim",
     config = function()
@@ -41,7 +43,13 @@ return {
   },
 
   -- Installed Themes
-  { "folke/tokyonight.nvim", opts = { transparent = true } },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      -- transparent = true,
+      transparent = false,
+    },
+  },
   { "AlexvZyl/nordic.nvim", opts = { transparent = { bg = true, float = true } } },
   {
     "navarasu/onedark.nvim",
@@ -56,13 +64,25 @@ return {
   { "rebelot/kanagawa.nvim" },
   { "projekt0n/github-nvim-theme" },
   { "rose-pine/neovim" },
-  { "Mofiqul/vscode.nvim", opts = {
-    transparent = true,
-  } },
+  {
+    "Mofiqul/vscode.nvim",
+    opts = {
+      transparent = false,
+      -- transparent = true,
+    },
+  },
   { "oxfist/night-owl.nvim" },
   { "ramojus/mellifluous.nvim" },
   { "atmosuwiryo/vim-winteriscoming" },
   { "rmehri01/onenord.nvim" },
+  { "sainnhe/everforest" },
+  {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+  },
+  { "olivercederborg/poimandres.nvim" },
+  { "ramojus/mellifluous.nvim" },
+  { "ayu-theme/ayu-vim" },
   {
     "nvim-lualine/lualine.nvim",
     optional = true,
@@ -70,6 +90,15 @@ return {
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("supermaven"))
     end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      -- transparent_background = false,
+    },
   },
   require("treesitter-context").setup({
     -- multiwindow = true, -- Enable multiwindow support.
