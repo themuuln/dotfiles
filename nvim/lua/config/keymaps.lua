873c7a8e73bbba9602849eefaa8d3f594a94ba6a
themuuln <zerone.offical@gmail.com>
2025-04-18 17:57:17 +0800

    feat(nvim): new opts, keymaps added

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,7 +3,14 @@ local wk = require("which-key")
 vim.keymap.set("n", "<C-m>", "<C-i>", { noremap = true, silent = true })
 vim.keymap.set("n", "U", ":redo<CR>", { noremap = true, silent = true })
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = false })
-vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = false })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "J", "mzJ`z")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 wk.add({
   { "<leader>dd", "<cmd>FlutterDebug<cr>", desc = "Debug Flutter App", group = "Debug Flutter App", mode = "n" },
@@ -17,17 +24,46 @@ wk.add({
   { "<leader>Fs", "<cmd>FlutterRun<cr>", desc = "Run Flutter App", group = "Run Flutter App", mode = "n" },
   { "<leader>Fr", "<cmd>FlutterRestart<cr>", desc = "Restart Flutter App", group = "Restart Flutter App", mode = "n" },
   {
-    "<leader>Fy",
-    "<cmd>FlutterLogToggle<cr>",
-    desc = "Flutter Log Toggle",
-    group = "Flutter Log Toggle",
-    mode = "n",
+    "<leader>t",
+    group = "toggle",
+    expand = function()
+      return require("which-key.extras").expand.buf()
+    end,
   },
   {
-    "<leader>FY",
-    "<cmd>FlutterLogToggle<cr>",
-    desc = "Flutter Log Toggle",
-    group = "Flutter Log Toggle",
+    "<leader>tt",
+    function()
+      local cat = require("catppuccin")
+      cat.options.transparent_background = not cat.options.transparent_background
+      cat.compile()
+      vim.cmd.colorscheme(vim.g.colors_name)
+    end,
+    desc = "ui",
+    group = "Toggle Transparent Background",
     mode = "n",
   },
 })
+
+-- vim.api.nvim_set_keymap("n", "<leader>tf", "<Plug>PlenaryTestFile", { noremap = false, silent = false })
+
+-- vim.keymap.set("n", "=ap", "ma=ap'a")
+-- vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+
+-- vim.keymap.set("n", "<leader>vwm", function()
+--   require("vim-with-me").StartVimWithMe()
+-- end)
+-- vim.keymap.set("n", "<leader>svwm", function()
+--   require("vim-with-me").StopVimWithMe()
+-- end)
+
+-- vim.keymap.set("x", "<leader>P", [["_dP]])
+
+-- vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
+
+-- vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+
+-- vim.keymap.set("n", "<leader>ea", 'oassert.NoError(err, "")<Esc>F";a')
+
+-- vim.keymap.set("n", "<leader>ef", 'oif err != nil {<CR>}<Esc>Olog.Fatalf("error: %s\\n", err.Error())<Esc>jj')
+
+-- vim.keymap.set("n", "<leader>el", 'oif err != nil {<CR>}<Esc>O.logger.Error("error", "error", err)<Esc>F.;i')
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,20 +1,29 @@
-vim.g.snacks_animate = true
+vim.g.snacks_animate = false
 vim.o.scrolloff = 8
-vim.opt.cursorcolumn = true
 vim.opt.smartindent = true
 -- views can only be fully collapsed with the global statusline avante.nvim
 vim.opt.laststatus = 3
--- -- vim insert mode cursor style is now `block`
--- vim.opt.guicursor = ""
+-- vim insert mode cursor style is now `block`
+vim.opt.guicursor = ""
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "120"
 vim.opt.formatoptions:append({ "r" })
 vim.g.lazyvim_cmp = "blink.cmp"
 vim.opt.cursorcolumn = false
 
+-- debug coloring
 local namespace = vim.api.nvim_create_namespace("dap-hlng")
 vim.api.nvim_set_hl(namespace, "DapBreakpoint", { fg = "#eaeaeb", bg = "#ffffff" })
 vim.api.nvim_set_hl(namespace, "DapLogPoint", { fg = "#eaeaeb", bg = "#ffffff" })
 vim.api.nvim_set_hl(namespace, "DapStopped", { fg = "#eaeaeb", bg = "#ffffff" })
-
 vim.fn.sign_define(
   "DapBreakpoint",
   { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
