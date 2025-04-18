a934278e52a8d61d4f24445b6473af22e624113d
themuuln <zerone.offical@gmail.com>
2025-04-18 17:57:31 +0800

    chore(avante): minor changes

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -4,10 +4,9 @@ return {
     event = "VeryLazy",
     version = false,
     opts = {
-      provider = "gemini_pro",
+      provider = "gemini",
       gemini = {
         model = "gemini-2.0-flash",
-        -- model = "gemini-2.0-flash-thinking-exp-01-21",
         -- model = "gemini-2.5-pro-exp-03-25",
         api_key_name = "GEMINI_API",
         timeout = 60000,
@@ -19,24 +18,30 @@ return {
         },
       },
       vendors = {
-        mistral_small = {
-          __inherited_from = "openai",
-          endpoint = "https://openrouter.ai/api/v1",
-          api_key_name = "OPENROUTER_API",
-          model = "mistralai/mistral-small-3.1-24b-instruct:free",
-        },
+        -- mistral_small = {
+        --   __inherited_from = "openai",
+        --   endpoint = "https://openrouter.ai/api/v1",
+        --   api_key_name = "OPENROUTER_API",
+        --   model = "mistralai/mistral-small-3.1-24b-instruct:free",
+        -- },
         gemini_pro = {
           __inherited_from = "openai",
           endpoint = "https://openrouter.ai/api/v1",
           api_key_name = "OPENROUTER_API",
           model = "google/gemini-2.5-pro-exp-03-25:free",
         },
+        -- qwen_coder = {
+        --   __inherited_from = "openai",
+        --   endpoint = "https://openrouter.ai/api/v1/",
+        --   api_key_name = "OPENROUTER_API",
+        --   model = "qwen/qwen-2.5-coder-32b-instruct:free",
+        -- },
       },
       behaviour = {
         auto_suggestions = false,
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
-        auto_apply_diff_after_generation = true,
+        auto_apply_diff_after_generation = false,
         support_paste_from_clipboard = true,
         minimize_diff = true,
         enable_token_counting = false,
