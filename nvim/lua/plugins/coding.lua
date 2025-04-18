95669698f1792584b3729de750f03289a4168357
themuuln <zerone.offical@gmail.com>
2025-04-18 20:38:50 +0800

    feat(nvim): surround limit increased to 100

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,9 +1,18 @@
 return {
   {
     "akinsho/bufferline.nvim",
-    opts = { options = { mode = "tabs", show_buffer_close_icons = false, show_close_icon = false } },
+    opts = {
+      options = {
+        -- mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
   },
   { "mg979/vim-visual-multi" },
+  { "echasnovski/mini.surround", version = false, opts = {
+    n_lines = 100,
+  } },
   {
     "themuuln/crsnip.nvim",
     config = function()
