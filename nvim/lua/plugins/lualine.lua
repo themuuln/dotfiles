f28ff4e669362eb9c0d3a878ce51c69102ba6707
themuuln <zerone.offical@gmail.com>
2025-04-21 16:31:12 +0800

    fix(lualine): show buffers

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -72,48 +72,6 @@ return {
     },
     sections = {
       lualine_a = {
-        lualine_a = {
-          {
-            "tabs",
-            tab_max_length = 40, -- Maximum width of each tab. The content will be shorten dynamically (example: apple/orange -> a/orange)
-            max_length = vim.o.columns / 3, -- Maximum width of tabs component.
-            -- Note:
-            -- It can also be a function that returns
-            -- the value of `max_length` dynamically.
-            mode = 0, -- 0: Shows tab_nr
-            -- 1: Shows tab_name
-            -- 2: Shows tab_nr + tab_name
-
-            path = 0, -- 0: just shows the filename
-            -- 1: shows the relative path and shorten $HOME to ~
-            -- 2: shows the full path
-            -- 3: shows the full path and shorten $HOME to ~
-
-            -- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
-            use_mode_colors = false,
-
-            tabs_color = {
-              -- Same values as the general color option can be used here.
-              active = "lualine_{section}_normal", -- Color for active tab.
-              inactive = "lualine_{section}_inactive", -- Color for inactive tab.
-            },
-
-            show_modified_status = true, -- Shows a symbol next to the tab name if the file has been modified.
-            symbols = {
-              modified = "[+]", -- Text to show when the file is modified.
-            },
-
-            fmt = function(name, context)
-              -- Show + if buffer is modified in tab
-              local buflist = vim.fn.tabpagebuflist(context.tabnr)
-              local winnr = vim.fn.tabpagewinnr(context.tabnr)
-              local bufnr = buflist[winnr]
-              local mod = vim.fn.getbufvar(bufnr, "&mod")
-
-              return name .. (mod == 1 and " +" or "")
-            end,
-          },
-        },
         {
           "branch",
           icon = "",
@@ -122,44 +80,9 @@ return {
             return curBranch ~= "main" and curBranch ~= "master" and vim.bo.buftype == ""
           end,
         },
-        { -- file name & icon
-          function()
-            local maxLength = 30
-            local name = vim.fs.basename(vim.api.nvim_buf_get_name(0))
-            local modified = vim.api.nvim_buf_get_option(0, "modified")
-
-            if name == "" then
-              name = vim.bo.ft
-            end
-            if name == "" then
-              name = "---"
-            end
-
-            local displayName = #name < maxLength and name or vim.trim(name:sub(1, maxLength)) .. "…"
-            if modified then
-              displayName = displayName .. " ●"
-            end
-
-            local ok, icons = pcall(require, "mini.icons")
-            if not ok then
-              return displayName
-            end
-            local icon, _, isDefault = icons.get("file", name)
-            if isDefault then
-              icon = icons.get("filetype", vim.bo.ft)
-            end
-
-            return icon .. " " .. displayName
-          end,
-        },
       },
-      lualine_b = {
-        { "buffers" },
-      },
-      lualine_c = {
-        -- { require("personal-plugins.alt-alt").mostChangedFileStatusbar },
-        { countLspRefs },
-      },
+      lualine_b = { { "buffers" } },
+      lualine_c = { { countLspRefs } },
       lualine_x = {
         { -- recording status
           function()
