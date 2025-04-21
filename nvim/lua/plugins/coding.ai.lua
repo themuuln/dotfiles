1b4b2baa08dce3fb88696671b12dcb588dbaf6c8
themuuln <zerone.offical@gmail.com>
2025-04-21 16:58:11 +0800

    refactor: file rename, categorized

--- nvim/lua/plugins/coding.ai.lua
+++ nvim/lua/plugins/coding.ai.lua
@@ -33,28 +33,21 @@ return {
         auto_suggestions = false,
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
-        auto_apply_diff_after_generation = false,
-        support_paste_from_clipboard = true,
-        minimize_diff = true,
-        enable_token_counting = false,
+        auto_apply_diff_after_generation = true,
+        minimize_diff = false,
+        enable_token_counting = true,
         enable_cursor_planning_mode = true,
         enable_claude_text_editor_tool_mode = true,
       },
     },
     build = "make",
-    dependencies = {
-      {
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = { insert_mode = true },
-            use_absolute_path = true,
-          },
-        },
-      },
+  },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = { "markdown", "Avante" },
     },
+    ft = { "markdown", "Avante" },
   },
 }
