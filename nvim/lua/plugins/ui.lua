358b43dcd36fe5ec163f3838a04654772ae745c4
themuuln <themuuln0@gmail.com>
2025-04-26 23:04:35 +0800

    ui(lsp): hide inlay hints by default

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -4,4 +4,5 @@ return {
     trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
     mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
   }),
+  { "neovim/nvim-lspconfig", opts = { inlay_hints = { enabled = false } } },
 }
