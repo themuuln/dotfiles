c6171f26ac21f54d34a290312fee074f0249d2e2
themuuln <themuuln0@gmail.com>
2025-04-28 11:42:43 +0800

    chore(flutter-tools): unused configs remomved

--- nvim/lua/plugins/coding.flutter-tools.lua
+++ nvim/lua/plugins/coding.flutter-tools.lua
@@ -10,42 +10,11 @@ return {
           enabled = true,
           run_via_dap = true,
           exception_breakpoints = "default",
-          -- evaluate_to_string_in_debug_views = true,
-          evaluate_to_string_in_debug_views = false,
-          register_configurations = function(_)
-            local dap = require("dap")
-            dap.adapters.dart = {
-              type = "executable",
-              command = "/Users/ict/development/flutter/bin/flutter",
-              args = { "debug_adapter" },
-            }
-            dap.configurations.dart = {
-              {
-                type = "dart",
-                request = "launch",
-                name = "Launch Flutter",
-                dartSdkPath = "/Users/ict/development/flutter/bin/cache/dart-sdk/bin/dart",
-                flutterSdkPath = "/Users/ict/development/flutter/bin/flutter",
-                program = "${workspaceFolder}/lib/main.dart",
-                cwd = "${workspaceFolder}",
-                args = { "development" },
-              },
-              {
-                type = "dart",
-                request = "attach",
-                name = "Attach to Flutter",
-                dartSdkPath = "/Users/ict/development/flutter/bin/cache/dart-sdk/bin/dart",
-                flutterSdkPath = "/Users/ict/development/flutter/bin/flutter",
-                program = "${workspaceFolder}/lib/main.dart",
-                cwd = "${workspaceFolder}",
-                args = { "development" },
-              },
-            }
-          end,
+          evaluate_to_string_in_debug_views = true,
         },
         root_patterns = { ".git", "pubspec.yaml" },
-        flutter_path = "/Users/ict/development/flutter/bin/flutter",
-        widget_guides = { enabled = false },
+        flutter_path = os.getenv("HOME") .. "/development/flutter/bin/flutter",
+        widget_guides = { enabled = true },
         closing_tags = { highlight = "Comment", prefix = "// " },
         dev_log = { enabled = false },
         dev_tools = { autostart = false, auto_open_browser = false },
