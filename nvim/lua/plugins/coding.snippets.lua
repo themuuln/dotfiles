504c2dc6985c24bdb14f74114aa2cda04dde0427
themuuln <themuuln0@gmail.com>
2025-05-04 22:10:20 +0800

    feat(snippet): crsnip -> nvim-scissors

---
+++ nvim/lua/plugins/coding.snippets.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "chrisgrieser/nvim-scissors",
+    opts = {
+      jsonFormatter = "jq",
+    },
+    keys = {
+      {
+        "<leader>cB",
+        function()
+          require("scissors").editSnippet()
+        end,
+        desc = "Edit Snippets",
+      },
+      {
+        "<leader>cb",
+        mode = { "n", "v" },
+        function()
+          require("scissors").addNewSnippet()
+        end,
+        desc = "Add Snippets",
+      },
+    },
+  },
+}
