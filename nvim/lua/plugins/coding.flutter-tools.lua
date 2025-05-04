6c90d4feb6b8a00323e5aa536492ccabcbb4618d
themuuln <themuuln0@gmail.com>
2025-05-04 22:09:49 +0800

    chore(flutter-tools): remove duplicated settings

--- nvim/lua/plugins/coding.flutter-tools.lua
+++ nvim/lua/plugins/coding.flutter-tools.lua
@@ -15,7 +15,6 @@ return {
         root_patterns = { ".git", "pubspec.yaml" },
         flutter_path = os.getenv("HOME") .. "/development/flutter/bin/flutter",
         widget_guides = { enabled = true },
-        closing_tags = { highlight = "Comment", prefix = "// " },
         dev_log = { enabled = false },
         dev_tools = { autostart = false, auto_open_browser = false },
         lsp = {
