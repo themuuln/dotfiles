0d39acf2dddf7a1c5b88bbc03a2380e02786a5f4
themuuln <themuuln0@gmail.com>
2025-05-04 22:09:09 +0800

    perf(opts.lua): optimization

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,47 @@
+local go = vim.g
+local o = vim.opt
+
+-- Optimizations on startup
+vim.loader.enable()
+
+-- Personal Config and LazyVim global options
+go.lualine_info_extras = false
+go.lazygit_config = false
+go.lazyvim_cmp = "blink"
+go.lazyvim_picker = "snacks"
+
+-- Define leader key
+go.mapleader = " "
+go.maplocalleader = "\\"
+
+-- Autoformat on save (Global)
+go.autoformat = true
+
+-- Font
+go.gui_font_face = "JetBrainsMono Nerd Font"
+
+-- Root dir detection
+go.root_spec = {
+  "lsp",
+  { ".git", "lua", ".obsidian", "package.json", "Makefile", "go.mod", "cargo.toml", "pyproject.toml", "src" },
+  "cwd",
+}
+
+-- Disable annoying cmd line stuff
+o.showcmd = false
+o.laststatus = 3
+o.cmdheight = 0
+
+-- Smoothscroll
+if vim.fn.has("nvim-0.10") == 1 then
+  o.smoothscroll = true
+end
+
 vim.g.snacks_animate = true
 vim.o.scrolloff = 8
 vim.opt.smartindent = true
 vim.opt.laststatus = 3
-vim.opt.guicursor = ""
+-- vim.opt.guicursor = ""
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
