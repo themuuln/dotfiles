792e93ab5b86160078890f088572198f6aa1c56e
themuuln <themuuln0@gmail.com>
2025-05-05 12:24:21 +0800

    fix(obsidian): conceallevel

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,6 +2,6 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "json", "jsonc", "markdown" },
   callback = function()
-    vim.opt.conceallevel = 0
+    vim.opt.conceallevel = 1
   end,
 })
