644ba3a0a3a7bc23795cb5a91ff6403b8a870d4a
themuuln <themuuln0@gmail.com>
2025-05-12 11:48:49 +0800

    chore(nvim): remove dupe code block

--- nvim/lua/plugins/ai.avante.lua
+++ nvim/lua/plugins/ai.avante.lua
@@ -40,11 +40,4 @@ return {
     },
     build = "make",
   },
-  -- {
-  --   "MeanderingProgrammer/render-markdown.nvim",
-  --   opts = {
-  --     file_types = { "markdown", "Avante" },
-  --   },
-  --   ft = { "markdown", "Avante" },
-  -- },
 }
