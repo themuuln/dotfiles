9ef00276897e2bca4116e9595377e9af05af0588
themuuln <themuuln0@gmail.com>
2025-05-13 14:56:51 +0800

    feat(leetcode): fix picker

--- nvim/lua/plugins/util.leetcode.lua
+++ nvim/lua/plugins/util.leetcode.lua
@@ -1,5 +1,7 @@
 return {
-  "kawre/leetcode.nvim",
+  -- "kawre/leetcode.nvim",
+  -- change url to fork (snacks.picker support)
+  "BYT0723/leetcode.nvim",
   build = ":TSUpdate html",
   dependencies = { "MunifTanjim/nui.nvim" },
   opts = {},
