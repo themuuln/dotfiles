cf2430a4e6995d7d6b352cac90b688e0f16677d0
themuuln <themuuln0@gmail.com>
2025-05-21 15:26:35 +0800

    feat(gitlab): gitlab MR controller plugin added (gitlab.nvim)

--- nvim/lua/plugins/misc.git.lua
+++ nvim/lua/plugins/misc.git.lua
@@ -11,5 +11,18 @@ return {
       },
     },
   },
+  {
+    "harrisoncramer/gitlab.nvim",
+    build = function()
+      require("gitlab.server").build(true)
+    end, -- Builds the Go binary
+    config = function()
+      require("gitlab").setup({
+        auth_provider = function()
+          return os.getenv("GITLAB_TOKEN"), os.getenv("GITLAB_VIM_URL"), nil
+        end,
+      })
+    end,
+  },
   { "sindrets/diffview.nvim" },
 }
