5ed778b096bafcb437b7bcf416ef5a310d5f1248
themuuln <themuuln0@gmail.com>
2025-07-01 11:01:31 +0800

    feat: update nvim ui.themes plugin config

--- nvim/lua/plugins/ui.themes.lua
+++ nvim/lua/plugins/ui.themes.lua
@@ -1,7 +1,34 @@
 return {
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "catppuccin-mocha" },
+    opts = { colorscheme = "catppuccin" },
   },
   { "catppuccin/nvim", name = "catppuccin" },
+  { "AlexvZyl/nordic.nvim", name = "nordic" },
+  -- recolor devicons to match theme
+  {
+    "rachartier/tiny-devicons-auto-colors.nvim",
+    event = "UIEnter",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons", commit = "9154484705968658e9aab2b894d1b2a64bf9f83d" },
+    },
+    config = true,
+  },
+
+  -- main theme
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  -- {
+  --   "nickkadutskyi/jb.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     -- require("jb").setup({ transparent = true })
+  --     vim.cmd("colorscheme jb")
+  --   end,
+  -- },
 }
