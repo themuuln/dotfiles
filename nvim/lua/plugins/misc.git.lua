4ee23a7fed4e9e26ad3aa722c397ea2774ac2059
themuuln <themuuln0@gmail.com>
2025-08-07 16:51:17 +0800

    chore(nvim): clean up

--- nvim/lua/plugins/misc.git.lua
+++ nvim/lua/plugins/misc.git.lua
@@ -2,40 +2,20 @@ return {
   {
     "dinhhuy258/git.nvim",
     event = "BufReadPre",
-    opts = {
-      keymaps = {
-        -- Open blame window
-        blame = "<Leader>gb",
-        -- Open file/folder in git repository
-        browse = "<Leader>go",
-      },
-    },
   },
-  -- {
-  --   "lewis6991/gitsigns.nvim",
-  --   event = "LazyFile",
-  --   opts = {
-  --     current_line_blame = true,
-  --     current_line_blame_opts = {
-  --       delay = 0,
-  --       virt_text_priority = 100,
-  --       use_focus = true,
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "harrisoncramer/gitlab.nvim",
-  --   build = function()
-  --     require("gitlab.server").build(true)
-  --   end, -- Builds the Go binary
-  --   config = function()
-  --     require("gitlab").setup({
-  --       auth_provider = function()
-  --         return os.getenv("GITLAB_TOKEN"), os.getenv("GITLAB_VIM_URL"), nil
-  --       end,
-  --     })
-  --   end,
-  -- },
+  {
+    "harrisoncramer/gitlab.nvim",
+    build = function()
+      require("gitlab.server").build(true)
+    end, -- Builds the Go binary
+    config = function()
+      require("gitlab").setup({
+        auth_provider = function()
+          return os.getenv("GITLAB_TOKEN"), os.getenv("GITLAB_VIM_URL"), nil
+        end,
+      })
+    end,
+  },
   { "sindrets/diffview.nvim" },
   -- { "tpope/vim-fugitive" },
 }
