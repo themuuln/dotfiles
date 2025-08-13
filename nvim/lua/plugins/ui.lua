62c0c72ceb9a905d8b4795502a12d53e21900cde
themuuln <themuuln0@gmail.com>
2025-08-13 18:14:12 +0800

    feat(nvim): grvubox config updated

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -48,32 +48,19 @@ return {
   -- { "Everblush/everblush.nvim" },
   -- { "cryptomilk/nightcity.nvim" },
   { "navarasu/onedark.nvim" },
-  { "sainnhe/gruvbox-material" },
   {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      terminal_colors = true, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = false,
-    },
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("gruvbox-material")
+      -- vim.g.gruvbox_material_background = "hard"
+      -- vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_better_performance = 1
+      -- vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_transparent_background = 1
+    end,
   },
   -- { "projekt0n/github-nvim-theme" },
   -- { "sainnhe/everforest" },
