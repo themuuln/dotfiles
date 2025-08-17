84c1bb1f848e147866008268f62de3695967d540
themuuln <themuuln0@gmail.com>
2025-08-17 17:01:11 +0800

    feat(nvim/ai): avante enabled

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -18,66 +18,66 @@ return {
     },
     cmd = { "OpencodeSend", "OpencodeSwitchMode" },
   },
-  -- {
-  --   "yetone/avante.nvim",
-  --   event = "VeryLazy",
-  --   -- lazy = false,
-  --   -- version = false,
-  --   opts = {
-  --     provider = "flash_500",
-  --     behaviour = {
-  --       enable_token_counting = false,
-  --
-  --       -- claude model only
-  --       enable_cursor_planning_mode = false,
-  --       enable_claude_text_editor_tool_mode = false,
-  --     },
-  --     providers = {
-  --       flash_500 = {
-  --         __inherited_from = "gemini",
-  --         model = "gemini-2.5-flash",
-  --         api_key_name = "GEMINI_API",
-  --         timeout = 60000,
-  --         temperature = 0,
-  --         -- generationConfig = {
-  --         --   thinkingConfig = {
-  --         --     thinkingBudget = 1024,
-  --         --   },
-  --         -- },
-  --       },
-  --       flash_500_2 = {
-  --         __inherited_from = "gemini",
-  --         model = "gemini-2.5-flash-preview-04-17",
-  --         api_key_name = "GEMINI_API",
-  --         timeout = 60000,
-  --         temperature = 0,
-  --         -- generationConfig = {
-  --         --   thinkingConfig = {
-  --         --     thinkingBudget = 1024,
-  --         --   },
-  --         -- },
-  --       },
-  --     },
-  --   },
-  --   build = "make",
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "stevearc/dressing.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       "MeanderingProgrammer/render-markdown.nvim",
-  --       opts = {
-  --         file_types = { "markdown", "Avante" },
-  --       },
-  --       ft = { "markdown", "Avante" },
-  --     },
-  --   },
-  --   init = function()
-  --     -- Recommended option
-  --     -- views can only be fully collapsed with the global statusline
-  --     vim.opt.laststatus = 3
-  --   end,
-  -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    -- lazy = false,
+    -- version = false,
+    opts = {
+      provider = "pro",
+      behaviour = {
+        enable_token_counting = false,
+
+        -- claude model only
+        enable_cursor_planning_mode = false,
+        enable_claude_text_editor_tool_mode = false,
+      },
+      providers = {
+        flash = {
+          __inherited_from = "gemini",
+          model = "gemini-2.5-flash",
+          api_key_name = "GEMINI_API",
+          timeout = 60000,
+          temperature = 0,
+          -- generationConfig = {
+          --   thinkingConfig = {
+          --     thinkingBudget = 1024,
+          --   },
+          -- },
+        },
+        pro = {
+          __inherited_from = "gemini",
+          model = "gemini-2.5-pro",
+          api_key_name = "GEMINI_API",
+          timeout = 60000,
+          temperature = 0,
+          -- generationConfig = {
+          --   thinkingConfig = {
+          --     thinkingBudget = 1024,
+          --   },
+          -- },
+        },
+      },
+    },
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    init = function()
+      -- Recommended option
+      -- views can only be fully collapsed with the global statusline
+      vim.opt.laststatus = 3
+    end,
+  },
 }
