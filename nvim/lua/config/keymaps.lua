efb91df5a41605e728d8d55787ad94036c8d8049
themuuln <themuuln0@gmail.com>
2025-10-20 18:28:58 +0800

    feat(nvim/keymaps): remove lazy keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,4 @@
-local lazy = require("lazy")
 local wk = require("which-key")
-local keymap = vim.keymap
 local map = vim.keymap.set
 local dap = require("dap")
 
@@ -45,29 +43,6 @@ map("v", "-", function()
   require("dial.map").manipulate("decrement", "visual")
 end)
 
--- lazy related
-map("n", "<leader>l", "<Nop>")
-map("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
-map("n", "<leader>ld", function()
-  vim.fn.system({ "open", "https://lazyvim.org" })
-end, { desc = "LazyVim Docs" })
-map("n", "<leader>lr", function()
-  vim.fn.system({ "open", "https://github.com/LazyVim/LazyVim" })
-end, { desc = "LazyVim Repo" })
-map("n", "<leader>lx", "<cmd>LazyExtras<cr>", { desc = "Extras" })
-map("n", "<leader>lc", function()
-  LazyVim.news.changelog()
-end, { desc = "LazyVim Changelog" })
-map("n", "<leader>lu", function()
-  lazy.update()
-end, { desc = "Lazy Update" })
-map("n", "<leader>lC", function()
-  lazy.check()
-end, { desc = "Lazy Check" })
-map("n", "<leader>ls", function()
-  lazy.sync()
-end, { desc = "Lazy Sync" })
-
 wk.add({
   { "<leader>dd", "<cmd>FlutterDebug<cr>", desc = "Debug Flutter App", group = "Debug Flutter App", mode = "n" },
   {
@@ -84,12 +59,5 @@ wk.add({
     "<cmd>FlutterRestart<cr>",
     desc = "Restart Flutter App",
     mode = "n",
-  },
-  {
-    "<leader>t",
-    group = "toggle",
-    expand = function()
-      return require("which-key.extras").expand.buf()
-    end,
   },
 })
