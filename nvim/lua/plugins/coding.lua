68dafda7230e9bff6f0f9116e8b863997a87d495
themuuln <themuuln0@gmail.com>
2025-10-29 23:51:20 +0800

    feat(nvim): add fff.nvim and enable flutter-tools
    Neovim-д fff.nvim файл хайх плагин нэмэв. Мөн flutter-tools-г идэвхжүүлж Flutter хөгжүүлэлтийн дэмжлэгтэй болгов. LSP inlay hints-г идэвхгүй болгож өөрчлөв.

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -13,6 +13,29 @@ return {
     },
   },
 
+  {
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+      require("fff.download").download_or_build_binary()
+    end,
+    opts = {
+      debug = {
+        enabled = false,
+        show_scores = false,
+      },
+    },
+    lazy = false,
+    keys = {
+      {
+        "<leader><Space>",
+        function()
+          require("fff").find_files()
+        end,
+        desc = "FFFind files",
+      },
+    },
+  },
+
   -- {
   --   "chrisgrieser/nvim-scissors",
   --   opts = { jsonFormatter = "jq" },
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,60 +3,50 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = { "saghen/blink.cmp" },
     opts = {
-      inlay_hints = { enabled = true },
+      inlay_hints = { enabled = false },
       diagnostics = { virtual_text = { prefix = "icons" } },
-      capabilities = {
-        workspace = {
-          didChangeWatchedFiles = {
-            dynamicRegistration = false,
-          },
-        },
-      },
     },
   },
 
   { "stevearc/dressing.nvim" },
 
-  -- {
-  --   "nvim-flutter/flutter-tools.nvim",
-  --   lazy = true,
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "stevearc/dressing.nvim",
-  --   },
-  --   ft = { "dart" },
-  --   config = function()
-  --     require("flutter-tools").setup({
-  --       debugger = {
-  --         enabled = true,
-  --         run_via_dap = true,
-  --         exception_breakpoints = {},
-  --         -- evaluate_to_string_in_debug_views = false,
-  --       },
-  --       flutter_path = os.getenv("HOME") .. "/development/flutter/bin/flutter",
-  --       default_run_args = nil, -- Default options for run command (i.e `{ flutter = "--no-version-check" }`). Configured separately for `dart run` and `flutter run`.
-  --       -- widget_guides = { enabled = true },
-  --       dev_log = { enabled = false },
-  --       dev_tools = { autostart = false, auto_open_browser = false },
-  --       lsp = {
-  --         color = {
-  --           enabled = false,
-  --         },
-  --         settings = {
-  --           lineLength = 120,
-  --           showTodos = false,
-  --           completeFunctionCalls = false,
-  --           renameFilesWithClasses = "prompt",
-  --           enableSdkFormatter = true,
-  --           analysisExcludedFolders = {
-  --             os.getenv("HOME") .. "/development/flutter/packages",
-  --             vim.fn.expand("$Home/.pub-cache"),
-  --           },
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    ft = { "dart" },
+    config = function()
+      require("flutter-tools").setup({
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+          -- exception_breakpoints = {},
+          -- evaluate_to_string_in_debug_views = false,
+        },
+        flutter_path = os.getenv("HOME") .. "/development/flutter/bin/flutter",
+        default_run_args = nil, -- Default options for run command (i.e `{ flutter = "--no-version-check" }`). Configured separately for `dart run` and `flutter run`.
+        -- widget_guides = { enabled = true },
+        dev_log = { enabled = false },
+        dev_tools = { autostart = false, auto_open_browser = false },
+        lsp = {
+          settings = {
+            lineLength = 120,
+            showTodos = false,
+            completeFunctionCalls = false,
+            renameFilesWithClasses = "prompt",
+            enableSdkFormatter = true,
+            analysisExcludedFolders = {
+              os.getenv("HOME") .. "/development/flutter/packages",
+              vim.fn.expand("$Home/.pub-cache"),
+            },
+          },
+        },
+      })
+    end,
+  },
 
   { "stevearc/conform.nvim", opts = {
     exclude_filetypes = { "dart" },
