ae4c343167dd0a9c004dde8f4dc59f5e48c564ce
themuuln <themuuln0@gmail.com>
2026-01-16 10:56:40 +0800

    refactor(nvim): autocmds moved from init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,30 +3,3 @@ if vim.env.VSCODE then
 end
 
 require("config.lazy")
-
-------------------
--- CUSTOM MACRO --
-------------------
-
-local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
-
-vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
-vim.api.nvim_create_augroup("DartLogMacro", { clear = true })
-
--- select variable, `@l` to print that data on the next line
-vim.api.nvim_create_autocmd("FileType", {
-  group = "JSLogMacro",
-  pattern = { "javascript", "typescript", "typescriptreact", "javascriptreact" },
-  callback = function()
-    vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
-  end,
-})
-
--- select variable, `@l` to log that data on the next line
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("DartLogMacro", { clear = true }),
-  pattern = "dart",
-  callback = function()
-    vim.fn.setreg("l", 'yo' .. 'log(' .. esc .. 'pa, prefix: "' .. esc .. 'pa");' .. esc)
-  end,
-})
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -40,3 +40,41 @@ vim.api.nvim_create_autocmd("FileType", {
 --     end
 --   end,
 -- })
+
+-- Open gf in the previous window if in REPL
+vim.keymap.set("n", "gf", function()
+  local target = vim.fn.expand("<cfile>")
+  if vim.bo.filetype == "dap-repl" or vim.bo.filetype == "terminal" then
+    vim.cmd("wincmd p") -- Jump to previous window
+    vim.cmd("edit " .. target)
+  else
+    vim.cmd("normal! gf")
+  end
+end, { desc = "Smart gf for REPL" })
+
+------------------
+-- CUSTOM MACRO --
+------------------
+
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+
+vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+vim.api.nvim_create_augroup("DartLogMacro", { clear = true })
+
+-- select variable, `@l` to print that data on the next line
+vim.api.nvim_create_autocmd("FileType", {
+  group = "JSLogMacro",
+  pattern = { "javascript", "typescript", "typescriptreact", "javascriptreact" },
+  callback = function()
+    vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
+  end,
+})
+
+-- select variable, `@l` to log that data on the next line
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("DartLogMacro", { clear = true }),
+  pattern = "dart",
+  callback = function()
+    vim.fn.setreg("l", "yo" .. "log(" .. esc .. 'pa, prefix: "' .. esc .. 'pa");' .. esc)
+  end,
+})
