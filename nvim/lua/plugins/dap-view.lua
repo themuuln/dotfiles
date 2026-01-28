53d1821b5e69a54d1aeca850d158072f7a809fc9
themuuln <themuuln0@gmail.com>
2026-01-28 17:25:40 +0800

    feat(dap): replace nvim-dap-ui with nvim-dap-view

---
+++ nvim/lua/plugins/dap-view.lua
@@ -0,0 +1,10 @@
+return {
+  { "rcarriga/nvim-dap-ui", enabled = false },
+  {
+    "igorlfs/nvim-dap-view",
+    ---@module 'dap-view'
+    ---@type dapview.Config
+    opts = {},
+    ft = { "dart" },
+  },
+}
